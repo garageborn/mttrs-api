d57f9826e7c4b99ec6aa541633d37cf57fadd9ba
Andreas Diegues <andreas.diegues@gmail.com>
2016-05-16 03:55:36 -0300

    Novos feeds (revisar todos)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
 if Rails.env.development?
-  technology = Category.create(name: 'Technology')
-  business = Category.create(name: 'Business')
-  entertainment = Category.create(name: 'Entertainment')
+  world_news = Category.find_or_create_by(name: 'World News')
+  business = Category.find_or_create_by(name: 'Business')
+  technology = Category.find_or_create_by(name: 'Technology')
+  entertainment = Category.find_or_create_by(name: 'Entertainment')
+  humor = Category.find_or_create_by(name: 'Humor')
+  science = Category.find_or_create_by(name: 'Science')
+
+  Publisher.create(
+    name: 'CNN',
+    feeds: [
+      Feed.new(category: world_news, url: 'http://rss.cnn.com/rss/edition.rss')
+    ]
+  )
+
+  Publisher.create(
+    name: 'TechCrunch',
+    feeds: [
+      Feed.new(category: business, url: 'http://feeds.feedburner.com/TechCrunch/startups'),
+    ]
+  )
+
+  Publisher.create(
+    name: 'Forbes',
+    feeds: [
+      Feed.new(category: business, url: 'http://www.forbes.com/business/feed2/')
+    ]
+  )
 
   Publisher.create(
     name: 'Mashable',
@@ -13,9 +37,16 @@ if Rails.env.development?
   )
 
   Publisher.create(
-    name: 'TechCrunch',
+    name: 'BuzzFeed',
     feeds: [
-      Feed.new(category: business, url: 'http://feeds.feedburner.com/TechCrunch/startups'),
+      Feed.new(category: humor, url: 'https://www.buzzfeed.com/index.xml'),
+    ]
+  )
+
+  Publisher.create(
+    name: 'Popular Science',
+    feeds: [
+      Feed.new(category: science, url: 'http://www.popsci.com/rss.xml'),
     ]
   )
 end
