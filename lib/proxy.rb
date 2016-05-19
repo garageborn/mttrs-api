59394e533f775df27d246fc83bdb34117cc6c964
Alexandre Bini <alexandrebini@gmail.com>
2016-05-19 16:17:56 -0300

    Proxies!

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,27 +1,19 @@
-class Proxy
-  URL = 'http://gimmeproxy.com/api/getProxy?supportsHttps=true'.freeze
+module Proxy
+  autoload :GimmeProxy, './lib/proxy/gimme_proxy'
 
+  class << self
+    def request(url, options = {}, method: :get, max_tries: 3)
+      with_retries(max_tries: max_tries) do
+        if proxy = sources.sample.current
+          options[:http_proxyaddr] = proxy[:host]
+          options[:http_proxyport] = proxy[:port]
+        end
+        HTTParty.send(method, url, options)
+      end
+    end
 
-
-  def request
-    HTTParty.get(URL)
-  end
-
-  private
-
-  def at_rate_limit
-    request.code == 429
-  end
-
-  def rate_limit
-    request.headers['x-ratelimit-limit']
-  end
-
-  def rate_limit_remaining
-    request.headers['x-ratelimit-remaining']
-  end
-
-  def next_rate_limit_window
-    1.minute.from_now
+    def sources
+      [GimmeProxy.instance]
+    end
   end
 end
--- lib/social_share/facebook.rb
+++ lib/social_share/facebook.rb
@@ -1,7 +1,7 @@
 module SocialShare
   class Facebook
     def self.count(url)
-      request = HTTParty.get("http://graph.facebook.com/?id=#{ url }")
+      request = Proxy.request("http://graph.facebook.com/?id=#{ url }")
       request.parsed_response.try(:[], 'shares').to_i
     rescue StandardError
     end
