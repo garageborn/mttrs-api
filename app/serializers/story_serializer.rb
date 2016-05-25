c7fb1b4fdf462779f39e351450c8348e68678f07
Alexandre Bini <alexandrebini@gmail.com>
2016-05-24 22:44:40 -0300

    fix story_serializer

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,4 +1,4 @@
 class StorySerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :content, :url, :image_source_url, :publisher,
-             :created_at, :updated_at
+             :total_social, :created_at, :updated_at
 end
