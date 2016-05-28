ef43ff0b3e896dccc6e5987bf8d356855abb4e99
Alexandre Bini <alexandrebini@gmail.com>
2016-05-27 21:24:32 -0300

    Features/omit query string (#12)
    * Omit query string
    
    * Omit query string
    
    * Omit query string

--- config/initializers/instruments/proxy_logger_instrument.rb
+++ config/initializers/instruments/proxy_logger_instrument.rb
@@ -1,20 +1,23 @@
 def logger
-  return if ENV['LOGGLY_TOKEN'].blank?
-  @logger ||= Logglier.new(
-    "https://logs-01.loggly.com/inputs/#{ ENV['LOGGLY_TOKEN'] }/tag/proxy/",
-    threaded: true,
-    format: :json
-  )
+  if Rails.env.production? && ENV['LOGGLY_TOKEN'].present?
+    @logger ||= Logglier.new(
+      "https://logs-01.loggly.com/inputs/#{ ENV['LOGGLY_TOKEN'] }/tag/proxy/",
+      threaded: true,
+      format: :json
+    )
+  else
+    @logger ||= Logger.new("#{ Rails.root }/log/proxy.log")
+  end
 end
 
 def debug_log(type, payload)
   action = payload.delete(:action)
   payload[:exception].try(:delete, :backtrace)
-  Rails.logger.send(type, "Proxy.#{ action } (#{ type }): #{ payload }")
+  logger.send(type, "Proxy.#{ action } (#{ type }): #{ payload }")
 end
 
 ActiveSupport::Notifications.subscribe('proxy.logger') do |_name, _start, _finish, _id, payload|
   type = payload.delete(:type) || :info
-  next debug_log(type, payload) if Rails.env.development? || logger.nil?
+  next debug_log(type, payload) if Rails.env.development?
   logger.send(type, payload)
 end
