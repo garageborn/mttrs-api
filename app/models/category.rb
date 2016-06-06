c65100518b3436861b6ce0bba25368038fc0bccd
Alexandre Bini <alexandrebini@gmail.com>
2016-06-06 18:48:45 -0300

    Category Matcher

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ActiveRecord::Base
 
   has_and_belongs_to_many :stories
   has_many :category_matchers, inverse_of: :category, dependent: :destroy
-  has_many :feeds, inverse_of: :category
+  has_many :feeds, inverse_of: :category, dependent: :nullify
   has_many :publishers, through: :stories
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
@@ -14,10 +14,7 @@ class Category < ActiveRecord::Base
     joins(:stories).group('categories.id').order('count(categories.id) desc')
   }
 
-  before_destroy do
-    feeds.clear
-    stories.clear
-  end
+  before_destroy { stories.clear }
 
   friendly_id :name, use: %i(slugged finders)
 end
