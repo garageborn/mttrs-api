b1e3201de8cc1db543c5d7f41dbf3a9b84bbaac6
Alexandre Bini <alexandrebini@gmail.com>
2016-06-20 23:24:52 -0300

    sanitizer

--- lib/extract/strategies/parser/language.rb
+++ lib/extract/strategies/parser/language.rb
@@ -3,9 +3,18 @@ module Extract
     module Parser
       class Language < Base
         def value
-          Utils::Language.find(
-            meta_value("itemprop='inLanguage'") || document.xpath('/html').attribute('lang').value
-          )
+          Utils::Language.find(in_language || html_language)
+        end
+
+        private
+
+        def in_language
+          meta_value("itemprop='inLanguage'")
+        end
+
+        def html_language
+          attribute = document.xpath('/html').attribute('lang')
+          return attribute.value if attribute.present?
         end
       end
     end
