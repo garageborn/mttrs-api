6daec6d87d90966e6519e1761fd7f59dee90ff4c
Alexandre Bini <alexandrebini@gmail.com>
2016-06-20 21:09:14 -0300

    sanitize on save

--- app/models/concerns/sanitizer.rb
+++ app/models/concerns/sanitizer.rb
@@ -3,12 +3,20 @@ module Concerns
     extend ActiveSupport::Concern
 
     included do
-      def self.sanitizer(*attributes)
-        attributes.each do |attribute|
-          define_method("#{ attribute }=") do |value|
-            self[attribute] = ActionView::Base.full_sanitizer.sanitize(value.to_s).strip
+      class << self
+        def sanitizer(*attributes)
+          before_validation do
+            attributes.each { |attribute| sanitize_attribute(attribute, self[attribute]) }
+          end
+
+          attributes.each do |attribute|
+            define_method("#{ attribute }=") { |value| sanitize_attribute(attribute, value) }
           end
         end
+      end
+
+      def sanitize_attribute(attribute, value)
+        self[attribute] = ActionView::Base.full_sanitizer.sanitize(value.to_s).strip
       end
     end
   end
