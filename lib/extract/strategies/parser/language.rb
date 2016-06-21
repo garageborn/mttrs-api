13b8061ab575ee6b97bfd40be4c8c7e09671ac97
Alexandre Bini <alexandrebini@gmail.com>
2016-06-20 23:22:14 -0300

    Revert "parser language fix"
    This reverts commit e5866ee3837796e79ad1476dd6e1d84dbdf99f0b.

--- lib/extract/strategies/parser/language.rb
+++ lib/extract/strategies/parser/language.rb
@@ -3,18 +3,9 @@ module Extract
     module Parser
       class Language < Base
         def value
-          Utils::Language.find(in_language || html_language)
-        end
-
-        private
-
-        def in_language
-          meta_value("itemprop='inLanguage'")
-        end
-
-        def html_language
-          attribute = document.xpath('/html').attribute('lang')
-          return attribute.value if attribute.present?
+          Utils::Language.find(
+            meta_value("itemprop='inLanguage'") || document.xpath('/html').attribute('lang').value
+          )
         end
       end
     end
