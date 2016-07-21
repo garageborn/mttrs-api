2e4d56528453ca29c3e3891ed8b757dd525367ed
Alexandre Bini <alexandrebini@gmail.com>
2016-07-21 12:21:12 -0300

    join fix

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,8 +3,7 @@ class Story < ApplicationRecord
   include Concerns::ParseDate
 
   has_many :categories, -> { distinct }, through: :links
-  has_many :links, inverse_of: :story, dependent: :nullify,
-           after_remove: :refresh!, after_add: :refresh!
+  has_many :links, inverse_of: :story, dependent: :nullify, after_remove: :refresh!, after_add: :refresh!
   has_many :publishers, -> { distinct }, through: :links
   has_one :main_link, -> { where(main: true) }, class_name: 'Link'
 
@@ -16,7 +15,7 @@ class Story < ApplicationRecord
   scope :last_month, -> { published_since(1.month.ago) }
   scope :last_week, -> { published_since(1.week.ago) }
   scope :order_by_links_count, lambda {
-    joins(:links).group('stories.id').order('count(links.id) desc')
+    joins(:links).group(:id).order('count(links.id) desc')
   }
   scope :popular, -> { order(total_social: :desc) }
   scope :published_at, lambda { |date|
@@ -24,13 +23,21 @@ class Story < ApplicationRecord
     published_between(date.at_beginning_of_day, date.end_of_day)
   }
   scope :published_between, lambda { |start_at, end_at|
-    joins(:main_link).where(links: { published_at: parse_date(start_at)..parse_date(end_at) })
+    joins(:main_link).where(
+      links: { main: true, published_at: parse_date(start_at)..parse_date(end_at) }
+    )
   }
   scope :published_since, lambda { |date|
-    joins(:main_link).where('links.published_at >= ?', parse_date(date))
+    joins(:main_link).where(
+      links: { main: true, published_at: parse_date(date)..Float::INFINITY }
+    )
   }
-  scope :publisher_slug, -> (slug) { joins(:publishers).where(publishers: { slug: slug }) }
-  scope :recent, -> { joins(:main_link).order('links.published_at desc') }
+  scope :publisher_slug, lambda { |slug|
+    joins(:publishers).group(:id).where(publishers: { slug: slug })
+  }
+  scope :recent, lambda {
+    joins(:main_link).where(links: { main: true }).order('links.published_at desc')
+  }
 
   def refresh!(_link = nil)
     return destroy if links.blank?
