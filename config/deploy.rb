94060984bb40e2fce450fca68206ab23926ee116
Alexandre Bini <alexandrebini@gmail.com>
2016-07-26 13:13:53 -0300

    assets on production

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{ fetch(:rbenv_path) } RBENV_VERSION=#{ fetch(:rbenv_ruby) } \
                    #{ fetch(:rbenv_path) }/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
+set :rbenv_map_bins, %w{ rake gem bundle ruby rails sidekiq sidekiqctl }
 set :rbenv_roles, :all
 
 # sidekiq
