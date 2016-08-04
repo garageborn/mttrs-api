e745463c8d4df437cccade645e321e41e76ca594
Alexandre Bini <alexandrebini@gmail.com>
2016-08-04 16:44:27 -0300

    build

--- .docker/build.sh
+++ .docker/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # if [[ -e ~/docker/image.tar ]]; then docker load -i ~/docker/image.tar; fi
+mkdir -p ~/docker/repo
+git clone -b master git@github.com:garageborn/mttrs-api.git ~/docker/repo
 
 docker build \
   --tag mttrs-api \
-  --file .docker/Dockerfile .
+  --file ~/docker/repo/.docker/Dockerfile .
 
-mkdir -p ~/docker; docker save mttrs-api > ~/docker/image.tar
+docker save mttrs-api > ~/docker/image.tar
