4e13255fc5bba2ce7458802f40b06fc4c77483d7
Alexandre Bini <alexandrebini@gmail.com>
2016-08-08 12:08:17 -0300

    ActiveRecord With Connection (#34)

--- lib/utils.rb
+++ lib/utils.rb
@@ -3,6 +3,7 @@ module Utils
   autoload :BinaryStringSerializer, './lib/utils/binary_string_serializer'
   autoload :Language, './lib/utils/language'
   autoload :OpenStructParser, './lib/utils/open_struct_parser'
+  autoload :Thread, './lib/utils/thread'
   autoload :UrlDiscovery, './lib/utils/url_discovery'
   autoload :UrlFetcher, './lib/utils/url_fetcher'
   autoload :UserAgent, './lib/utils/user_agent'
