36dc121778aaef32e58654bb9785b023d51f662c
Alexandre Bini <alexandrebini@gmail.com>
2016-08-10 00:22:13 -0300

    Proxy as a service (#35)

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -10,7 +10,7 @@ class Proxy
   PROXY_URL = ENV.fetch('PROXY_URL', 'http://proxy.mtt.rs')
 
   def self.request(url, options: {}, verb: :get)
-    params = { url: url, options: options }
+    params = options.to_h.merge(url: url)
     HTTParty.send(verb, PROXY_URL, query: params)
   end
 end
