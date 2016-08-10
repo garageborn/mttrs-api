0d71aa9d32f53de0fdf444c77cf3a910fddf8590
Alexandre Bini <alexandrebini@gmail.com>
2016-08-10 00:22:13 -0300

    Proxy as a service (#35)

---
+++ lib/proxy.rb
@@ -0,0 +1,16 @@
+class Proxy
+  RESCUE_FROM = [
+    EOFError,
+    Errno::ECONNREFUSED,
+    Errno::ECONNRESET,
+    Net::HTTPServerException,
+    Timeout::Error
+  ].freeze
+  DEFAULT_TIMEOUT = 10
+  PROXY_URL = ENV.fetch('PROXY_URL', 'http://proxy.mtt.rs')
+
+  def self.request(url, options: {}, verb: :get)
+    params = { url: url, options: options }
+    HTTParty.send(verb, PROXY_URL, query: params)
+  end
+end
--- lib/social/strategies/direct/pinterest.rb
+++ lib/social/strategies/direct/pinterest.rb
@@ -4,13 +4,13 @@ module Social
       class Pinterest
         class << self
           def count(url)
-            request = Proxy::Request.run(
+            request = Proxy.request(
               "http://api.pinterest.com/v1/urls/count.json?callback=nul&url=#{ url }"
             )
             return unless request && request.success?
             return if request.parsed_response.blank?
             parse(request.body).try(:[], 'count').to_i
-          rescue *Proxy::Request::RESCUE_FROM
+          rescue *Proxy::RESCUE_FROM
           end
 
           private
