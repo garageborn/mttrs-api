048e56e0a4ea86c82ec2225e047cec2f664c58ec
Alexandre Bini <alexandrebini@gmail.com>
2016-08-09 22:42:45 -0300

    env vars

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,21 @@
 require 'sidekiq'
 
 REDIS_URL = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379')
+REDIS_NAMESPACE = "mttrs-api_#{ ENV['RAILS_ENV'] }"
 
 Sidekiq.configure_client do |config|
   config.redis = {
     url: REDIS_URL,
-    size: ENV.fetch('SIDEKIQ_REDIS_CLIENT_SIZE', 1).to_i
+    size: ENV.fetch('SIDEKIQ_REDIS_CLIENT_SIZE', 1).to_i,
+    namespace: REDIS_NAMESPACE
   }
 end
 
 Sidekiq.configure_server do |config|
   config.redis = {
     url: REDIS_URL,
-    size: ENV.fetch('SIDEKIQ_REDIS_SERVER_SIZE', 30).to_i
+    size: ENV.fetch('SIDEKIQ_REDIS_SERVER_SIZE', 30).to_i,
+    namespace: REDIS_NAMESPACE
   }
 end
 
--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,12 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch('MTTRS_MAX_THREADS') { 5 }.to_i
+threads_count = ENV.fetch('MTTRS_API_MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port ENV.fetch('PORT') { 3000 }
+port ENV.fetch('MTTRS_API_PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -21,7 +21,7 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch('MTTRS_WEB_CONCURRENCY') { 2 }
+workers ENV.fetch('MTTRS_API_WEB_CONCURRENCY') { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
