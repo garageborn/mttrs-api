5e0d2c2531d8410708b36e3d3814b0781cee4c33
Alexandre Bini <alexandrebini@gmail.com>
2016-08-12 16:32:43 -0300

    proxy timeout

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,9 +1,9 @@
 class Proxy
-  DEFAULT_TIMEOUT = 10
+  TIMEOUT = 60
   PROXY_URL = ENV['PROXY_INTERNAL_URL'] || ENV['PROXY_URL'] || 'http://proxy.mtt.rs'
 
   def self.request(url, options: {}, verb: :get)
     params = options.to_h.merge(url: url)
-    HTTParty.send(verb, PROXY_URL, query: params)
+    HTTParty.send(verb, PROXY_URL, query: params, timeout: TIMEOUT )
   end
 end
