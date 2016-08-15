4ddd2c64aa2f21888c1eff70648675cf4e57042b
Alexandre Bini <alexandrebini@gmail.com>
2016-08-15 14:25:29 -0300

    remove PROXY_INTERNAL_URL

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,6 +1,6 @@
 class Proxy
   TIMEOUT = 60
-  PROXY_URL = ENV['PROXY_INTERNAL_URL'] || ENV['PROXY_URL'] || 'http://proxy.mtt.rs'
+  PROXY_URL = ENV['PROXY_URL'] || 'http://proxy.mtt.rs'
 
   def self.request(url, options: {}, verb: :get)
     params = options.to_h.merge(url: url)
