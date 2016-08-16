405ce23002bfdbf8822db1e1f7e56fc2a44630b5
Alexandre Bini <alexandrebini@gmail.com>
2016-08-16 16:56:24 -0300

    linkedin fix

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -3,7 +3,7 @@ class Proxy
   PROXY_URL = ENV['PROXY_URL'] || 'http://proxy.mtt.rs'
 
   def self.request(url, options: {}, verb: :get)
-    params = options.to_h.merge(url: url)
-    HTTParty.send(verb, PROXY_URL, query: params, timeout: TIMEOUT)
+    query = { url: url, options: options }
+    HTTParty.send(verb, PROXY_URL, query: query, timeout: TIMEOUT)
   end
 end
