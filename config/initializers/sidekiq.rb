b5315ea39d25b2ef19c69a26c3c8b4fa761597d7
Alexandre Bini <alexandrebini@gmail.com>
2016-08-16 22:33:56 -0300

    redis network_timeout

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,10 @@
 require 'sidekiq'
 require 'redis-namespace'
 
-REDIS_OPTIONS = {
-  url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
-  namespace: "mttrs-api:#{ ENV['RAILS_ENV'] }",
-  network_timeout: ENV.fetch('REDIS_TIMEOUT', 5)
-}
+REDIS_URL = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379')
+REDIS_NAMESPACE = "mttrs-api:#{ ENV['RAILS_ENV'] }"
+REDIS_TIMEOUT = ENV.fetch('REDIS_TIMEOUT', 10_000).to_i / 1_000 # seconds
+REDIS_OPTIONS = { url: REDIS_URL, namespace: REDIS_NAMESPACE, network_timeout: REDIS_TIMEOUT }
 
 Sidekiq.configure_client do |config|
   config.redis = REDIS_OPTIONS.merge(size: ENV.fetch('SIDEKIQ_REDIS_CLIENT_SIZE', 1).to_i)
