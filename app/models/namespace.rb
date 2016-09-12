fb4b43f647d266173c7be021c716a8ae99ad6a5d
Alexandre Bini <alexandrebini@gmail.com>
2016-09-12 09:51:15 -0300

    test fix

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -1,11 +1,6 @@
 class Namespace < ApplicationRecord
   include Concerns::Filterable
 
-  has_many :stories, through: :links
-
-  scope :order_by_links_count, lambda {
-    joins(:links).group('namespaces.id').order('count(namespaces.id) desc')
-  }
   scope :order_by_slug, -> { order(:slug) }
 
   def categories
