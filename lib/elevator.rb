49612199c4fb1a7adbe4f2eee00778ad846ce145
Alexandre Bini <alexandrebini@gmail.com>
2016-09-18 14:51:05 -0300

    elevator

--- lib/elevator.rb
+++ lib/elevator.rb
@@ -2,7 +2,7 @@ require 'apartment/elevators/generic'
 
 class Elevator < Apartment::Elevators::Generic
   def parse_tenant_name(request)
-    header_tenant(request.env['X-Tenant']) || path_tenant(request.path) || default_tenant
+    header_tenant(request) || path_tenant(request) || default_tenant
   end
 
   private
@@ -11,11 +11,13 @@ class Elevator < Apartment::Elevators::Generic
     Apartment.tenant_names.first
   end
 
-  def header_tenant(header)
-    Apartment.tenant_names.find { |tenant_name| tenant_name.downcase == header }
+  def header_tenant(request)
+    Apartment.tenant_names.find do |tenant_name|
+      tenant_name.downcase == request.env['HTTP_X_TENANT']
+    end
   end
 
-  def path_tenant(path)
+  def path_tenant(request)
     # todo
   end
 end
