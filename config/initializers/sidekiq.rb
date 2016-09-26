bdc317b1094161ffaa035d9cd857d9854fb418a9
Alexandre Bini <alexandrebini@gmail.com>
2016-09-26 20:28:55 -0300

    hiredis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,6 +6,7 @@ REDIS_URL = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379').freeze
 REDIS_NAMESPACE = ENV['MTTRS_API_REDIS_NAMESPACE'].freeze
 REDIS_TIMEOUT = ENV.fetch('REDIS_TIMEOUT', 10_000).to_i / 1_000 # seconds
 REDIS_OPTIONS = {
+  driver: :hiredis,
   url: REDIS_URL,
   namespace: REDIS_NAMESPACE,
   network_timeout: REDIS_TIMEOUT
