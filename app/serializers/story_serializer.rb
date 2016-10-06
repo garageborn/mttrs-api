e4a33a5062e38d25da0d47fee1dafefafb1ba0b9
Alexandre Bini <alexandrebini@gmail.com>
2016-10-06 11:59:30 -0300

    story attrs

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,5 +1,7 @@
 class StorySerializer < ActiveModel::Serializer
-  attributes :id, :total_social, :created_at, :updated_at
+  attributes :id, :title, :url, :image_source_url, :total_social, :published_at
+
+  has_many :links
   has_one :main_link
   has_many :other_links
 end
