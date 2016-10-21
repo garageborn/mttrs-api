1527f149b130d8166f5887b7eec31370db77e2ab
Alexandre Bini <alexandrebini@gmail.com>
2016-10-21 11:11:19 -0200

    remove loggly

--- config/initializers/loggly.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.production? && ENV['LOGGLY_TOKEN'].present?
-  loggly = Logglier.new(
-    "https://logs-01.loggly.com/inputs/#{ ENV['LOGGLY_TOKEN'] }/tag/rails/",
-    threaded: true,
-    format: :json
-  )
-  Rails.logger.extend(ActiveSupport::Logger.broadcast(loggly))
-end
