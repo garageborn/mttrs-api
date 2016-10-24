20e5b99e8525a22480944692bfce455a9edd4752
Alexandre Bini <alexandrebini@gmail.com>
2016-10-24 19:57:48 -0200

    story summary (#59)

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,5 +1,5 @@
 class StorySerializer < ActiveModel::Serializer
-  attributes :id, :total_social
+  attributes :id, :total_social, :headline, :summary
   has_one :main_link
   has_many :other_links
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 19) do
+ActiveRecord::Schema.define(version: 20) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,8 @@ ActiveRecord::Schema.define(version: 19) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.datetime "published_at",             null: false
+    t.string   "headline"
+    t.text     "summary"
     t.index ["total_social"], name: "index_stories_on_total_social", using: :btree
   end
 
