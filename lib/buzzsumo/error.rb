ef28d2bc25dde3e7860ffb214b5296012bffac16
Alexandre Bini <alexandrebini@gmail.com>
2016-10-25 15:50:59 -0200

    RATELIMIT_HEADERS

--- lib/buzzsumo/error.rb
+++ lib/buzzsumo/error.rb
@@ -1,12 +1,16 @@
 module Buzzsumo
   class Error < StandardError
     attr_reader :path, :headers
+    RATELIMIT_HEADERS = %w(
+      x-ratelimit-reset
+      x-ratelimit-limit
+      x-ratelimit-remaining
+      x-ratelimit-month-remaining
+    ).freeze
 
     def initialize(response)
       @path = response.request.last_uri.to_s
-      @headers = response.headers.select do |key, _value|
-        %w(x-ratelimit-reset x-ratelimit-limit x-ratelimit-month-remaining).include?(key)
-      end
+      @headers = response.headers.select { |key, _value| RATELIMIT_HEADERS.include?(key) }
       @status = response.code
       super(response.error)
     end
