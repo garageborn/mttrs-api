bb8cef7e9f9b2196383daacef9ef868b1e774cf7
Alexandre Bini <alexandrebini@gmail.com>
2016-10-24 22:25:53 -0200

    publishers scope

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -10,6 +10,7 @@ QueryType = GraphQL::ObjectType.define do
 
   field :publishers, !types[PublisherType] do
     argument :limit, types.Int
+    argument :order_by_name, types.Boolean
     resolve -> (_obj, args, _ctx) { Publisher.filter(args) }
   end
 
