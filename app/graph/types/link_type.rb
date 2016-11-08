2d346803ef658fc5b9335dfe59a6a8081ea6342b
Alexandre Bini <alexandrebini@gmail.com>
2016-11-08 12:24:10 -0200

    Main Publisher Link (#63)
    * Main Publisher Link
    
    * DRY

--- app/graph/types/link_type.rb
+++ app/graph/types/link_type.rb
@@ -2,11 +2,12 @@ LinkType = GraphQL::ObjectType.define do
   name 'Link Type'
   description 'Link Type'
 
-  field :id, !types.ID
-  field :title, !types.String
-  field :url, !types.String
-  field :created_at, !types.Int
-  field :publisher, !PublisherType
   field :categories, types[CategoryType]
+  field :created_at, !types.Int
+  field :id, !types.ID
   field :image_source_url, types.String
+  field :publisher, !PublisherType
+  field :title, !types.String
+  field :total_social, !types.Int
+  field :url, !types.String
 end
--- app/graph/types/story_type.rb
+++ app/graph/types/story_type.rb
@@ -5,11 +5,26 @@ StoryType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :total_social, !types.Int
   field :created_at, !types.Int
-  field :main_link, !LinkType
-  field :other_links, types[LinkType]
   field :main_category, !CategoryType do
-    resolve -> (obj, _args, _ctx) {
-      obj.categories.first
+    resolve -> (obj, _args, _ctx) { obj.categories.first }
+  end
+  field :main_link, !LinkType do
+    argument :publisher_slug, types.String
+    resolve -> (obj, args, _ctx) {
+      publisher_slug = args['publisher_slug']
+      filter = args.to_h.except('publisher_slug')
+      return obj.main_link if publisher_slug.blank?
+      obj.main_publisher_link(publisher_slug)
+    }
+  end
+  field :other_links, types[LinkType] do
+    argument :popular, types.Boolean
+    argument :publisher_slug, types.String
+    resolve -> (obj, args, _ctx) {
+      publisher_slug = args['publisher_slug']
+      filter = args.to_h.except('publisher_slug')
+      return obj.other_links.filter(filter) if publisher_slug.blank?
+      obj.links.where.not(id: obj.main_publisher_link(publisher_slug)).filter(filter)
     }
   end
 end
