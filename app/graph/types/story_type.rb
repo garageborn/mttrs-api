98658036ccf9551acb3d20ec5be69f9c376aa3ee
Alexandre Bini <alexandrebini@gmail.com>
2016-11-08 12:04:12 -0200

    Main Publisher Link

--- app/graph/types/story_type.rb
+++ app/graph/types/story_type.rb
@@ -5,11 +5,27 @@ StoryType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :total_social, !types.Int
   field :created_at, !types.Int
-  field :main_link, !LinkType
-  field :other_links, types[LinkType]
   field :main_category, !CategoryType do
-    resolve -> (obj, _args, _ctx) {
-      obj.categories.first
+    resolve -> (obj, _args, _ctx) { obj.categories.first }
+  end
+  field :main_link, !LinkType do
+    argument :publisher_slug, types.String
+    resolve -> (obj, args, _ctx) {
+      publisher_slug = args['publisher_slug']
+      return obj.main_link if publisher_slug.blank?
+      filter = args.to_h.except('publisher_slug')
+      obj.links.publisher_slug(publisher_slug).popular.filter(filter).first
+    }
+  end
+  field :other_links, types[LinkType] do
+    argument :popular, types.Boolean
+    argument :publisher_slug, types.String
+    resolve -> (obj, args, _ctx) {
+      publisher_slug = args['publisher_slug']
+      return obj.other_links if publisher_slug.blank?
+      filter = args.to_h.except('publisher_slug')
+      main_link = obj.links.publisher_slug(publisher_slug).popular.filter(filter).first
+      obj.links.where.not(id: main_link).filter(filter)
     }
   end
 end
