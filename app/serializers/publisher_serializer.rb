cad137ffffcc3a07ce83c34558f66f8d133f0a1e
Alexandre Bini <alexandrebini@gmail.com>
2017-01-02 16:13:27 -0200

    Matchers

--- app/serializers/publisher_serializer.rb
+++
@@ -1,3 +0,0 @@
-class PublisherSerializer < ActiveModel::Serializer
-  attributes :id, :name, :slug, :icon_id
-end
--- app/serializers/story_serializer.rb
+++
@@ -1,5 +0,0 @@
-class StorySerializer < ActiveModel::Serializer
-  attributes :id, :total_social, :headline, :summary
-  has_one :main_link
-  has_many :other_links
-end
