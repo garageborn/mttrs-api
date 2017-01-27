df9fa4ca08c52a938766450901d8aed2b45217b9
Alexandre Bini <alexandrebini@gmail.com>
2017-01-27 16:03:27 -0200

    link cache

--- app/graph/types/link_type.rb
+++ app/graph/types/link_type.rb
@@ -6,10 +6,20 @@ LinkType = GraphQL::ObjectType.define do
   field :created_at, !types.Int
   field :id, !types.ID
   field :image_source_url, types.String
-  field :publisher, !PublisherType
+  field :publisher, !PublisherType do
+    resolve LinkType.cache.fetch(:publisher) { |_obj, _args, _ctx|
+      obj.publisher
+    }
+  end
   field :story, !StoryType
   field :title, !types.String
   field :total_social, !types.Int
   field :url, !types.String
   field :slug, !types.String
+end
+
+GraphqlCache.define(LinkType) do |config|
+  config.base_key do |obj, _args, _ctx|
+    "link_type/#{ obj.id }"
+  end
 end
