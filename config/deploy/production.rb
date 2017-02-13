3a5d5695285ef1cdca82a22bbd2b2eb77776bfa8
Alexandre Bini <alexandrebini@gmail.com>
2017-02-13 10:48:59 -0200

    worker

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
 server 'app01.server.garageborn.com', user: 'garageborn', roles: %w{app web}
-server 'app02.server.garageborn.com', user: 'garageborn', roles: %w{app web db scheduler worker}
+server 'app02.server.garageborn.com', user: 'garageborn', roles: %w{app web db}
+server 'worker01.server.garageborn.com', user: 'garageborn', roles: %w{scheduler worker}
