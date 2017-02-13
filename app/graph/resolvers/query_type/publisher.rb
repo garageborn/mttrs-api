b4dc5a8e7c00ba9fa7f201f53e1b85eecd667f49
Alexandre Bini <alexandrebini@gmail.com>
2017-02-13 12:26:00 -0200

    Resolvers

--- app/graph/resolvers/query_type/publisher.rb
+++ app/graph/resolvers/query_type/publisher.rb
@@ -1,6 +1,6 @@
 module Resolvers
   module QueryType
-    class Publishers < Base
+    class Publisher < Base
       def resolve
         cache_for(:publisher).expires_in 15.minutes
         Publisher.find(args['slug'])
