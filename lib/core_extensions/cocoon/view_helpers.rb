004c8d2c7b93506d2b923311785779d1e59538a3
Alexandre Bini <alexandrebini@gmail.com>
2017-02-13 12:26:00 -0200

    Resolvers

--- lib/core_extensions/cocoon/view_helpers.rb
+++ lib/core_extensions/cocoon/view_helpers.rb
@@ -2,12 +2,12 @@
 module CoreExtensions
   module Cocoon
     module ViewHelpers
-      def render_association(association, f, new_object, form_name, render_options={}, custom_partial=nil)
+      def render_association(association, f, new_object, form_name, render_options = {}, custom_partial = nil)
         partial = get_partial_path(custom_partial, association)
         locals =  render_options.delete(:locals) || {}
         ancestors = f.class.ancestors.map{|c| c.to_s}
         method_name = ancestors.include?('SimpleForm::FormBuilder') ? :simple_fields_for : (ancestors.include?('Formtastic::FormBuilder') ? :semantic_fields_for : :fields_for)
-        f.send(method_name, association, new_object, {:child_index => "new_#{association}"}.merge(render_options)) do |builder|
+        f.send(method_name, association, new_object, { child_index: "new_#{association}" }.merge(render_options)) do |builder|
           partial_options = {form_name.to_sym => builder, :dynamic => true}.merge(locals)
           render(view: partial, locals: partial_options).html_safe
         end
