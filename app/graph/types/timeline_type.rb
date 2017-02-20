9e9b414be8a4a899cb49652f36c54edeb55002ca
Alexandre Bini <alexandrebini@gmail.com>
2017-02-20 11:59:36 -0300

    Update timeline_type.rb

--- app/graph/types/timeline_type.rb
+++ app/graph/types/timeline_type.rb
@@ -1,13 +1,6 @@
 TimelineType = GraphQL::ObjectType.define do
   name 'TimelineType'
 
-  # argument :limit, types.Int
-  # argument :category_slug, types.String
-  # argument :limit, types.Int
-  # argument :popular, types.Boolean
-  # argument :publisher_slug, types.String
-  # argument :recent, types.Boolean
-
   field :date, types.Int
   field :stories, types[StoryType] do
     resolve Resolvers::TimelineType::Stories
