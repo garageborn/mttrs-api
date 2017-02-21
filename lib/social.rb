c34bcf1115505ea666dfea7c1cbbbf7e474883b0
Alexandre Bini <alexandrebini@gmail.com>
2017-02-21 11:20:25 -0300

    Remove social_counter lock (#84)

--- lib/social.rb
+++ lib/social.rb
@@ -3,12 +3,35 @@ module Social
   autoload :Strategies, './lib/social/strategies'
 
   class << self
-    def count(url, strategy:)
-      if strategy.to_sym == :buzzsumo
-        Strategies::Buzzsumo.count(url)
-      else
-        Strategies::Direct.count(url)
-      end
+    def count(url)
+      entry = find_entry(url)
+      return if entry.blank?
+      counters_from_entry(entry)
+    end
+
+    def counters_from_entry(entry)
+      return if entry.blank?
+      Social::Counters.new(
+        facebook: entry[:total_facebook_shares],
+        linkedin: entry[:linkedin_shares],
+        twitter: entry[:twitter_shares],
+        pinterest: entry[:pinterest_shares],
+        google_plus: entry[:google_plus_shares]
+      )
+    end
+
+    private
+
+    def find_entries(url)
+      query = { q: url, num_results: 1 }
+      response = ::Buzzsumo::Articles.get(query: query)
+      return unless response.success?
+      return if response.parsed_response.blank?
+      response.parsed_response.results.to_a
+    end
+
+    def find_entry(url)
+      find_entries(url).to_a.detect { |entry| url == entry[:url] }
     end
   end
 end
--- lib/social/strategies.rb
+++
@@ -1,6 +0,0 @@
-module Social
-  module Strategies
-    autoload :Buzzsumo, './lib/social/strategies/buzzsumo'
-    autoload :Direct, './lib/social/strategies/direct'
-  end
-end
