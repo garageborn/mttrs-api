5a35815a5bf1559fffd7682468ec7a82507e45a6
Alexandre Bini <alexandrebini@gmail.com>
2017-03-02 00:08:31 -0300

    strip content

--- lib/extract/content.rb
+++ lib/extract/content.rb
@@ -4,7 +4,11 @@ module Extract
   class Content < Base
     def value
       readability = Readability::Document.new(document, tags: [], remove_empty_nodes: true)
+<<<<<<< Updated upstream
       return readability.content unless readability.content.blank?
+=======
+      return Utils::StripAttributes.run(readability.content) unless readability.content.blank?
+>>>>>>> Stashed changes
     end
   end
 end
