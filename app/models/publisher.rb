72284781d4d30d9b2cf866d180d2c82fdc363932
Alexandre Bini <alexandrebini@gmail.com>
2017-03-06 21:44:46 -0300

    fix category_matchers/new

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -13,7 +13,7 @@ class Publisher < ApplicationRecord
   friendly_id :name, use: %i(slugged finders)
 
   scope :available_on_current_tenant, -> { with_tenant_language }
-  scope :order_by_name, -> { order('unaccent(publishers.name)') }
+  scope :order_by_name, -> { order(:name) }
   scope :with_stories, -> { joins(:stories).group('publishers.id') }
   scope :with_tenant_language, lambda {
     next all if current_tenant_languages.blank?
