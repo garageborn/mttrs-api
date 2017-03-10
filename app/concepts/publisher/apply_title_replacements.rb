68feb07c7c0ed234747f896de7384b01a1a30c03
Alexandre Bini <alexandrebini@gmail.com>
2017-03-09 21:20:28 -0300

    next if new_title == link.title

--- app/concepts/publisher/apply_title_replacements.rb
+++ app/concepts/publisher/apply_title_replacements.rb
@@ -6,6 +6,7 @@ class Publisher
       return if model.title_replacements.blank? || model.links.blank?
       model.links.find_each do |link|
         new_title = model.title_replacements.apply(link.title)
+        next if new_title == link.title
         link.update_attributes(title: new_title)
       end
     end
