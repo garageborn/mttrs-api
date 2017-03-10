29ece88c292eb09d258375e8b588f011a24ed575
Alexandre Bini <alexandrebini@gmail.com>
2017-03-09 22:03:54 -0300

    fix specs

--- spec/factories/story_factory.rb
+++ spec/factories/story_factory.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :story do
     total_social { Faker::Number.number(5).to_i }
     published_at { Time.zone.now }
+    category
   end
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Story do
   it { should have_db_column(:total_social).with_options(null: false, default: 0) }
   it { should have_db_column(:updated_at).with_options(null: false) }
 
-  it { should have_many(:categories).through(:links) }
+  it { should belong_to(:category) }
   it { should have_many(:story_links).inverse_of(:story).dependent(:destroy) }
   it { should have_many(:links).through(:story_links) }
   it { should have_many(:publishers).through(:links) }
