b037ca34acf7be5f0863a7ef1daa561975b05499
Alexandre Bini <alexandrebini@gmail.com>
2017-03-20 16:38:48 -0300

    Features/link update story (#104)
    * Link::UpdateStory after full fetch
    
    * Link::UpdateStory after full fetch

---
+++ app/concepts/link/update_story.rb
@@ -0,0 +1,21 @@
+class Link
+  class UpdateStory < Operation
+    def process(params)
+      Apartment::Tenant.each do
+        Link::UpdateStory::Tenant.run(id: params[:id])
+      end
+    end
+
+    class Tenant < Operation
+      action :find
+      delegate :story, to: :model
+
+      def process(_params)
+        return if story.blank? || story.links.size != 1
+
+        story.update_attributes(category: model.category, published_at: model.published_at)
+      end
+
+    end
+  end
+end
