2d9ae89cfe6a1b12adb971a54964f1daa141c4cc
Alexandre Bini <alexandrebini@gmail.com>
2017-03-22 10:55:34 -0300

    fix Link::CreateAccess

--- app/concepts/link/create_access.rb
+++ app/concepts/link/create_access.rb
@@ -1,6 +1,7 @@
 class Link
   class CreateAccess < Operation
     def process(_params)
+      return if model.blank?
       Access::Create.run(access: { accessable_type: 'Link', accessable_id: model.id }) do |op|
         break if model.story.blank?
         UpdateStoryAccessesJob.perform_async(model.id, op.model.date)
