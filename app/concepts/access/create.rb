8b99fa48db252e46bdbb0908daf1c85e7a46237a
Alexandre Bini <alexandrebini@gmail.com>
2017-03-31 14:44:26 -0300

    access:create with_retries

--- app/concepts/access/create.rb
+++ app/concepts/access/create.rb
@@ -1,21 +1,21 @@
 class Access
   class Create < Operation
     MAX_RETRIES = 3
+    RESCUE_FROM = [
+      ActiveRecord::RecordNotUnique
+    ].freeze
 
     def process(params)
-      with_retries(max_tries: MAX_RETRIES, rescue: ActiveRecord::RecordNotUnique) do
-        validate(params[:access]) do
+      reload_model_handler = proc { reload_model! }
+      validate(params[:access]) do
+        with_retries(max_tries: MAX_RETRIES, handler: reload_model_handler, rescue: RESCUE_FROM) do
           save
         end
       end
     end
 
     def model!(params)
-      Access.where(
-        accessable_type: params[:access][:accessable_type],
-        accessable_id: params[:access][:accessable_id],
-        date: params[:access][:date]
-      ).first_or_initialize
+      find_or_initialize(params)
     end
 
     def params!(params)
@@ -39,6 +39,18 @@ class Access
 
     def access_params
       @params[:access]
+    end
+
+    def reload_model!
+      self.model = find_or_initialize(@params)
+    end
+
+    def find_or_initialize(params)
+      Access.where(
+        accessable_type: params[:access][:accessable_type],
+        accessable_id: params[:access][:accessable_id],
+        date: params[:access][:date]
+      ).first_or_initialize
     end
   end
 end
