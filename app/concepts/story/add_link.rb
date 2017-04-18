9995d89ca5b2b599574e480834d8d6053cc39749
Alexandre Bini <alexandrebini@gmail.com>
2017-04-18 10:41:16 -0300

    priorize stories with summaries on Story::AddLink

--- app/concepts/story/add_link.rb
+++ app/concepts/story/add_link.rb
@@ -9,13 +9,10 @@ class Story
       return if model == link.story || !valid_link?
 
       if link.missing_story?
-        link.with_lock { link.update_attributes(story: model) }
+        add_link
       else
         merge_story
       end
-
-      ::BlockedStoryLink.where(story: model, link: link).destroy_all
-      RefreshStoryJob.perform_async(model.id)
     end
 
     private
@@ -28,11 +25,22 @@ class Story
       link.belongs_to_current_tenant? && link.category == model.category
     end
 
+    def add_link
+      link.with_lock { link.update_attributes(story: model) }
+      ::BlockedStoryLink.where(story: model, link: link).destroy_all
+      RefreshStoryJob.perform_async(model.id)
+    end
+
     def merge_story
-      other_story = link.story
+      stories = [model, link.story]
+      destination = stories.detect { |story| story.summary.present? } || stories.first
+      other_story = stories.detect { |story| story != destination }
+
       other_story.links.find_each do |similar_link|
-        similar_link.update_attributes(story: model)
+        similar_link.update_attributes(story: destination)
       end
+      ::BlockedStoryLink.where(story: destination, link: link).destroy_all
+      RefreshStoryJob.perform_async(destination.id)
       Story::Destroy.run(id: other_story.id)
     end
 
