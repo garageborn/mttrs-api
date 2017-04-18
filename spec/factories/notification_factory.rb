87ab05a02ab671dcf29d82205dbdda9b47486619
Alexandre Bini <alexandrebini@gmail.com>
2017-04-18 09:33:24 -0300

    fix notification factory

--- spec/factories/notification_factory.rb
+++ spec/factories/notification_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :notification do
     notificable { build(:link) }
+    segment { 'mttrs_us test' }
     title { Faker::Lorem.sentence }
     message { Faker::Lorem.paragraph }
     image_url { generate(:url) }
