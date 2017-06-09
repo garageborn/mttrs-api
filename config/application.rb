850128cabac9398efc456ce829a32d27535a6878
Alexandre Bini <alexandrebini@gmail.com>
2017-06-09 15:27:00 -0300

    AWS_DEFAULT_REGION

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module Mttrs
         bucket: ENV.fetch('S3_BUCKET_NAME'),
         access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
         secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-        s3_region: ENV.fetch('AWS_REGION'),
+        s3_region: ENV.fetch('AWS_DEFAULT_REGION'),
       }
     }
   end
