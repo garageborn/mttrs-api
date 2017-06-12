45306af1d1937129078826bd48cf69aa213aba3e
Alexandre Bini <alexandrebini@gmail.com>
2017-06-12 11:09:25 -0300

    Remove worker

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server 'app01.server.garageborn.com', user: 'garageborn', roles: %w{app web worker}
 server 'app02.server.garageborn.com', user: 'garageborn', roles: %w{app web worker}
-server 'app03.server.garageborn.com', user: 'garageborn', roles: %w{app web db scheduler worker}
+server 'app03.server.garageborn.com', user: 'garageborn', roles: %w{app web worker db scheduler}
 # server 'worker01.server.garageborn.com', user: 'garageborn', roles: %w{db scheduler worker}
