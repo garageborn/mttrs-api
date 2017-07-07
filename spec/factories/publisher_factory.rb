5eabcfbab7d1eca1cfac6371c98c6892ee0d8082
Alexandre Bini <alexandrebini@gmail.com>
2017-07-07 11:40:28 -0300

    fix factory

--- spec/factories/publisher_factory.rb
+++ spec/factories/publisher_factory.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :publisher do
     name { generate(:sentence) }
+    icon_file_name { 'image.png' }
+    icon_content_type { 'image' }
+    icon_file_size { 1024 }
+    icon_updated_at { Time.now }
   end
 end
